f6296ddd97f4e5719cbfc4d901f518d2c6871f8c
Dashark <dasha.hu@gmail.com>
2016-07-24 13:45:58 +0800

    XML batch
    I can program more intellegently. divided automatically by day.
    I can program in Python.

---
+++ examples/heatmap-legend/combine.bat
@@ -0,0 +1,58 @@
+@echo off
+chcp 65001
+
+mkdir C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\BI
+copy C:\Users\huzong\Downloads\鱼眼热图\captrue1_20160708*.xml C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\BI
+C:\Users\huzong\Downloads\python-3.5.2-embed-amd64\python.exe C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\combine.py
+move C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\BI C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\captrue1_20160708
+
+mkdir C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\BI
+copy C:\Users\huzong\Downloads\鱼眼热图\captrue2_20160708*.xml C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\BI
+C:\Users\huzong\Downloads\python-3.5.2-embed-amd64\python.exe C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\combine.py
+move C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\BI C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\captrue2_20160708
+
+mkdir C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\BI
+copy C:\Users\huzong\Downloads\鱼眼热图\captrue3_20160708*.xml C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\BI
+C:\Users\huzong\Downloads\python-3.5.2-embed-amd64\python.exe C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\combine.py
+move C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\BI C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\captrue3_20160708
+
+mkdir C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\BI
+copy C:\Users\huzong\Downloads\鱼眼热图\captrue4_20160708*.xml C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\BI
+C:\Users\huzong\Downloads\python-3.5.2-embed-amd64\python.exe C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\combine.py
+move C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\BI C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\captrue4_20160708
+
+mkdir C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\BI
+copy C:\Users\huzong\Downloads\鱼眼热图\captrue1_20160709*.xml C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\BI
+C:\Users\huzong\Downloads\python-3.5.2-embed-amd64\python.exe C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\combine.py
+move C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\BI C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\captrue1_20160709
+
+mkdir C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\BI
+copy C:\Users\huzong\Downloads\鱼眼热图\captrue2_20160709*.xml C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\BI
+C:\Users\huzong\Downloads\python-3.5.2-embed-amd64\python.exe C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\combine.py
+move C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\BI C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\captrue2_20160709
+
+mkdir C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\BI
+copy C:\Users\huzong\Downloads\鱼眼热图\captrue3_20160709*.xml C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\BI
+C:\Users\huzong\Downloads\python-3.5.2-embed-amd64\python.exe C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\combine.py
+move C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\BI C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\captrue3_20160709
+
+mkdir C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\BI
+copy C:\Users\huzong\Downloads\鱼眼热图\captrue4_20160709*.xml C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\BI
+C:\Users\huzong\Downloads\python-3.5.2-embed-amd64\python.exe C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\combine.py
+move C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\BI C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\captrue4_20160709
+
+mkdir C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\BI
+copy C:\Users\huzong\Downloads\鱼眼热图\captrue1_20160710*.xml C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\BI
+C:\Users\huzong\Downloads\python-3.5.2-embed-amd64\python.exe C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\combine.py
+move C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\BI C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\captrue1_20160710
+
+mkdir C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\BI
+copy C:\Users\huzong\Downloads\鱼眼热图\captrue1_20160711*.xml C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\BI
+C:\Users\huzong\Downloads\python-3.5.2-embed-amd64\python.exe C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\combine.py
+move C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\BI C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\captrue1_20160711
+
+mkdir C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\BI
+copy C:\Users\huzong\Downloads\鱼眼热图\captrue1_20160712*.xml C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\BI
+C:\Users\huzong\Downloads\python-3.5.2-embed-amd64\python.exe C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\combine.py
+move C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\BI C:\Users\huzong\Documents\GitHub\heatmap.js\examples\heatmap-legend\captrue1_20160712
+
